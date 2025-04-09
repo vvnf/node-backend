7815df7d1a2742110d47da583003773e0aa9fab6
vvnf <vivian3050@gmail.com>
2025-04-09 21:55:48 +0200

    all setup complete

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(cookieParser())
 
 
 //routes import
-// import userRouter from './routes/user.routes.js'
+ import userRouter from './routes/user.routes.js'
 // import healthcheckRouter from "./routes/healthcheck.routes.js"
 // import tweetRouter from "./routes/tweet.routes.js"
 // import subscriptionRouter from "./routes/subscription.routes.js"
@@ -28,7 +28,7 @@ app.use(cookieParser())
 
 //routes declaration
 // app.use("/api/v1/healthcheck", healthcheckRouter)
-// app.use("/api/v1/users", userRouter)
+ app.use("/api/v1/users", userRouter)
 // app.use("/api/v1/tweets", tweetRouter)
 // app.use("/api/v1/subscriptions", subscriptionRouter)
 // app.use("/api/v1/videos", videoRouter)
